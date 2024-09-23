82397025b694086cd9cf8f3575bed04984d35141
soska <soska2318@itmo.ru>
2024-09-23 11:37:48 +0300

    asd

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-import pytest
-
-@
 def test_upper():
     assert 'foo'.upper() == 'FOO'
 
 
 def test_isupper():
     assert 'FOO'.isupper()
-
 
 def test_failed_upper():
     assert 'fo'.upper() == 'FO'
